d128c3469b7e2798c08ee95fdb2772aa3a3d96cd
Joker Apple <734719526@qq.com>
2020-12-29 14:14:03 +0800

    FacebookSDK 添加

---
+++ Assets/Scripts/Component/Manager/SDK/AnalyticsSdkManager.cs
@@ -0,0 +1,287 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AnalyticsSdkManager : BaseManager
+{
+    public FacebookSdkManager manager_FacebookSdk;
+
+    private void Awake()
+    {
+        AutoLinkManager();
+        Init();
+    }
+
+    public void Init()
+    {
+        manager_FacebookSdk.Init();
+    }
+
+    public void LogEvent(string name)
+    {
+#if UNITY_EDITOR
+        // Debug.LogError("打点事件:" + name);
+#endif
+
+        // Debug.LogError("打点事件:" + name);
+
+        //FirebaseSdkManager.Instance.LogEvent(name);
+        manager_FacebookSdk.LogEvent(name);
+        //////GameAnalytics.NewDesignEvent(name);
+        //////AppsFlyer.sendEvent(name, null);
+    }
+
+
+
+    public void LogEvent(string name, string parameterName, long parameterValue)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+#endif
+        // Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+        //FirebaseSdkManager.Instance.LogEvent(name, parameterName, parameterValue);
+        manager_FacebookSdk.LogEvent(name, parameterName, parameterValue);
+
+        //////if (parameterName.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName + ":" + parameterValue);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName, parameterValue);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName, parameterValue.ToString() } });
+    }
+
+    public void LogEvent(string name, string parameterName, string parameterValue)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+#endif
+        //FirebaseSdkManager.Instance.LogEvent(name, parameterName, parameterValue);
+        manager_FacebookSdk.LogEvent(name, parameterName, parameterValue);
+        // Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+        //////if (parameterName.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName + ":" + parameterValue);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName, parameterValue);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName, parameterValue.ToString() } });
+    }
+    public void LogEvent(string name, string parameterName1, string parameterValue1, string parameterName2, string parameterValue2)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+#endif
+        //FirebaseSdkManager.Instance.LogEvent(name, parameterName1, parameterValue1, parameterName2, parameterValue2);
+        manager_FacebookSdk.LogEvent(name, parameterName1, parameterValue1, parameterName2, parameterValue2);
+        // Debug.LogError($"打点事件:{name}  {parameterName1}  {parameterValue1} {parameterName2} {parameterValue2}");
+        //////if (parameterName.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName + ":" + parameterValue);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName, parameterValue);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName, parameterValue.ToString() } });
+    }
+    public void LogEvent(string name, string parameterName1, long parameterValue1, string parameterName2, long parameterValue2)
+    {
+#if UNITY_EDITOR
+        // Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2}");
+#endif
+
+        //FirebaseSdkManager.Instance.LogEvent(name, new Parameter(parameterName1, parameterValue1), new Parameter(parameterName2, parameterValue2));
+
+        manager_FacebookSdk.LogEvent(name, parameterName1, parameterValue1, parameterName2, parameterValue2);
+        // Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2}");
+        //////if (parameterName1.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1 + ":" + parameterValue1);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1, parameterValue1);
+        //////}
+
+        //////if (parameterName2.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2 + ":" + parameterValue2);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2, parameterValue2);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName1, parameterValue1.ToString() }, { parameterName2, parameterValue2.ToString() } });
+    }
+
+    public void LogEvent(string name, string parameterName1, long parameterValue1, string parameterName2, long parameterValue2, string parameterName3, long parameterValue3)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2} {parameterName3} {parameterValue3}");
+#endif
+
+        //FirebaseSdkManager.Instance.LogEvent(name, new Parameter(parameterName1, parameterValue1), new Parameter(parameterName2, parameterValue2), new Parameter(parameterName3, parameterValue3));
+        manager_FacebookSdk.LogEvent(name, parameterName1, parameterValue1, parameterName2, parameterValue2, parameterName3, parameterValue3);
+        // Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2} {parameterName3} {parameterValue3}");
+        //////if (parameterName1.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1 + ":" + parameterValue1);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1, parameterValue1);
+        //////}
+
+        //////if (parameterName2.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2 + ":" + parameterValue2);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2, parameterValue2);
+        //////}
+
+        //////if (parameterName3.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName3 + ":" + parameterValue3);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName3, parameterValue3);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName1, parameterValue1.ToString() }, { parameterName2, parameterValue2.ToString() }, { parameterName3, parameterValue3.ToString() } });
+    }
+
+    public void LogEvent(string name, string parameterName1, long parameterValue1, string parameterName2, long parameterValue2, string parameterName3, double parameterValue3)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2} {parameterName3} {parameterValue3}");
+#endif
+
+        //FirebaseSdkManager.Instance.LogEvent(name, new Parameter(parameterName1, parameterValue1), new Parameter(parameterName2, parameterValue2), new Parameter(parameterName3, parameterValue3));
+
+        manager_FacebookSdk.LogEvent(name, parameterName1, parameterValue1, parameterName2, parameterValue2, parameterName3, parameterValue3);
+
+        // Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2} {parameterName3} {parameterValue3}");
+
+        //////if (parameterName1.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1 + ":" + parameterValue1);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1, parameterValue1);
+        //////}
+
+        //////if (parameterName2.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2 + ":" + parameterValue2);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2, parameterValue2);
+        //////}
+
+        //////GameAnalytics.NewDesignEvent(name + ":" + parameterName3, (float)parameterValue3);
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName1, parameterValue1.ToString() }, { parameterName2, parameterValue2.ToString() }, { parameterName3, parameterValue3.ToString() } });
+    }
+
+    public void LogEvent(string name, string parameterName1, long parameterValue1, string parameterName2, long parameterValue2, string parameterName3, long parameterValue3, string parameterName4, long parameterValue4)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2} {parameterName3} {parameterValue3} {parameterName4} {parameterValue4}");
+#endif
+
+        //FirebaseSdkManager.Instance.LogEvent(name, new Parameter(parameterName1, parameterValue1), new Parameter(parameterName2, parameterValue2), new Parameter(parameterName3, parameterValue3), new Parameter(parameterName4, parameterValue4));
+
+        manager_FacebookSdk.LogEvent(name, parameterName1, parameterValue1, parameterName2, parameterValue2, parameterName3, parameterValue3, parameterName4, parameterValue4);
+        // Debug.LogError($"打点事件:{name} {parameterName1}  {parameterValue1} {parameterName2}  {parameterValue2} {parameterName3} {parameterValue3} {parameterName4} {parameterValue4}");
+        //////if (parameterName1.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1 + ":" + parameterValue1);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName1, parameterValue1);
+        //////}
+
+        //////if (parameterName2.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2 + ":" + parameterValue2);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName2, parameterValue2);
+        //////}
+
+        //////if (parameterName3.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName3 + ":" + parameterValue3);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName3, parameterValue3);
+        //////}
+
+        //////if (parameterName4.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName4 + ":" + parameterValue4);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName4, parameterValue4);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName1, parameterValue1.ToString() }, { parameterName2, parameterValue2.ToString() }, { parameterName3, parameterValue3.ToString() }, { parameterName4, parameterValue4.ToString() } });
+    }
+
+
+    public void LogEvent(string name, string parameterName, int parameterValue)
+    {
+#if UNITY_EDITOR
+        //Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+#endif
+
+        //FirebaseSdkManager.Instance.LogEvent(name, parameterName, parameterValue);
+
+        manager_FacebookSdk.LogEvent(name, parameterName, parameterValue);
+        // Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+        //////if (parameterName.EndsWith("Id"))
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName + ":" + parameterValue);
+        //////}
+        //////else
+        //////{
+        //////    GameAnalytics.NewDesignEvent(name + ":" + parameterName, parameterValue);
+        //////}
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName, parameterValue.ToString() } });
+    }
+
+
+
+    public void LogEvent(string name, string parameterName, double parameterValue)
+    {
+#if UNITY_EDITOR
+        // Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+#endif
+        // Debug.LogError($"打点事件:{name}  {parameterName}  {parameterValue}");
+        //FirebaseSdkManager.Instance.LogEvent(name, parameterName, parameterValue);
+
+        manager_FacebookSdk.LogEvent(name, parameterName, parameterValue);
+
+        //////GameAnalytics.NewDesignEvent(name + ":" + parameterName, (float)parameterValue);
+
+        //////AppsFlyer.sendEvent(name, new Dictionary<string, string> { { parameterName, parameterValue.ToString() } });
+    } 
+}
---
+++ Assets/Scripts/Component/Manager/SDK/FacebookSdkManager.cs
@@ -0,0 +1,105 @@
+﻿using Facebook.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookSdkManager : BaseManager
+{
+    public void Init()
+    {
+        FB.Init(() =>
+        {
+            Debug.Log("FB Init success");
+            FB.Mobile.SetAutoLogAppEventsEnabled(true);
+            FB.ActivateApp();
+        });
+
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            FB.Init(() =>
+            {
+                FB.Mobile.SetAutoLogAppEventsEnabled(true);
+                FB.ActivateApp();
+            });
+        }
+    }
+
+    public void LogEvent(string name, string parameterName1, object parameterValue1, string parameterName2, object parameterValue2)
+    {
+        if (!FB.IsInitialized)
+            return;
+        FB.LogAppEvent(name, null, new Dictionary<string, object>()
+        {
+            { parameterName1, parameterValue1 },
+            { parameterName2, parameterValue2 },
+        });
+    }
+
+    public void LogEvent(string name, string parameterName1, object parameterValue1, string parameterName2, object parameterValue2, string parameterName3, object parameterValue3)
+    {
+        if (!FB.IsInitialized)
+            return;
+        FB.LogAppEvent(name, null, new Dictionary<string, object>()
+        {
+            { parameterName1, parameterValue1 },
+            { parameterName2, parameterValue2 },
+            { parameterName3, parameterValue3 },
+        });
+    }
+    public void LogEvent(string name, string parameterName1, object parameterValue1, string parameterName2, object parameterValue2, string parameterName3, object parameterValue3, string parameterName4, object parameterValue4)
+    {
+        if (!FB.IsInitialized)
+            return;
+        FB.LogAppEvent(name, null, new Dictionary<string, object>()
+        {
+            { parameterName1, parameterValue1 },
+            { parameterName2, parameterValue2 },
+            { parameterName3, parameterValue3 },
+            { parameterName4, parameterValue4 },
+        });
+    }
+    public void LogEvent(string name, string parameterName, object parameterValue)
+    {
+        if (!FB.IsInitialized)
+            return;
+        FB.LogAppEvent(name, null, new Dictionary<string, object>()
+        {
+            { parameterName, parameterValue }
+        });
+    }
+
+    public void LogEvent(string name)
+    {
+        if (!FB.IsInitialized)
+            return;
+        FB.LogAppEvent(name, null, null);
+    }
+
+    public void LogEvent(string name, params object[] events)
+    {
+        if (!FB.IsInitialized)
+            return;
+        int count = events != null ? events.Length / 2 : 0;
+        if (count > 0)
+        {
+            Dictionary<string, object> param = new Dictionary<string, object>();
+            for (int i = 0; i < count; i++)
+            {
+                string key = events[i * 2] as string;
+                if (string.IsNullOrEmpty(key)) { Debug.LogError(" fetal error: " + name + ": key is not string:" + events[i * count]); continue; }
+                object value = events[i * 2 + 1];
+                param[key] = value;
+            }
+
+            FB.LogAppEvent(name, null, param);
+        }
+        else
+        {
+            FB.LogAppEvent(name, null, null);
+        }
+    }
+}
